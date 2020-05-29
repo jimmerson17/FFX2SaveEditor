0603ce4079f1e8ab1a090cdfeb9836ec852f2c6f
shmuckmonkey <45115507+shmuckmonkey@users.noreply.github.com>
2020-05-29 01:24:35 -0700

    including switch save class i don't use anyway

---
+++ FFX2SaveEditor/Saves/SwitchSave.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFX2SaveEditor.Saves
+{
+    /// <summary>
+    /// The raw file format of the PC version.
+    /// </summary>
+    public class SwitchSave : Ffx2Save
+    {
+        public string OriginalName;
+
+        public SwitchSave(string filename) : base(0x16270, 0x7988, 0x7cc8, 0x784C, 0x7855, 0x2234)
+        {
+            OriginalName = Path.GetFileName(filename);
+            ReadFile(new MemoryStream(File.ReadAllBytes(filename)));
+        }
+    }
+}
